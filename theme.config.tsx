fc81c562aa9b994937e8aadbd3567388d16b09b1
Dylan Molina <dylanivanmolina@gmail.com>
2022-12-12 22:14:30 -0600

    removed default documentation and wrote the documentation for Baro Capital

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Baro Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/barocapital',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/9Df4Z5Ej',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Baro Capital Official Documentation',
   },
 }
 
